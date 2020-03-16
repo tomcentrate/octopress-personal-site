c43c425c073a36e60f5ea53a4bc1bd2854612432
Tommy Lee <tlee@entertainmentbenefits.com>
2020-03-15 21:47:31 -0400

    Updates

--- src/components/SidebarCheckbox.js
+++
@@ -1,3 +0,0 @@
-import React from "react";
-
-export default props => <label htmlFor="sidebar-checkbox" className="sidebar-toggle" />
--- src/pages/about.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import MainContainer from "../components/MainContainer";
-export default () => {
-    return (
-        <MainContainer>
-            Hey I'm Tommy
-        </MainContainer>
-    )
-}
